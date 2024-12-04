1fb4be557f79dbf2a0c652babcfff7307928cad6
khokan <khokancrmt@gmail.com>
2024-12-04 19:09:15 +0600

    create post operation

--- index.js
+++ index.js
@@ -22,8 +22,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // const gymShedule = client.db('gym-shedule').collection('shedule');
+    const userCollection = client.db('gaming-user').collection('users');
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/users', async(req,res)=>{
+        const newUser = req.body;
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
